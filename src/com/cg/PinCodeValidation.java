234e26bba26084f2e5f22f2d65483b37ac527275
Arijit20 <ARIJIT DEY@LAPTOP-F4J8SDAU>
2020-09-28 18:37:11 +0530

    [Arijit]Updated : Validated Length of Pin Code

--- src/com/cg/PinCodeValidation.java
+++ src/com/cg/PinCodeValidation.java
@@ -1,10 +1,29 @@
 package com.cg;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import java.util.Scanner;
+
 public class PinCodeValidation {
+	
+	Scanner in = new Scanner(System.in);
+	public void pinCodeValidator() {
+		System.out.println("Enter your Pin Code :");
+		String pinCode = in.next();
+		Pattern pattern = Pattern.compile("^\\S{6}$");
+		Matcher matcher = pattern.matcher(pinCode);
+		Boolean b = matcher.find();
+		if (b)
+			System.out.println("Pin Code is Validated");
+		else
+			System.out.println("Please enter the correct Pin Code");
+	}
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
     System.out.println("Pin Code Validation Program");
+    PinCodeValidation pinCodeValidation = new PinCodeValidation();
+    pinCodeValidation.pinCodeValidator();
+    
 	}
 
 }
